73ec8d1ac1d6b779aad39957f69f01803c273e8a
closertb <closertb@sina.com>
2019-01-23 23:14:34 +0800

    feat: 配置打包为umd格式

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -1,5 +1,7 @@
 module.exports = (config) => {
   config.disableCSSModules = true;
   config.disableAntdStyle = true;
+  config.library = 'ffe-basic';
+  config.libraryTarget = 'umd';
   return config;
 };
