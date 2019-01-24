cce226382636fa49ee4bb2f42ddd5a936c8292eb
bo.tang3 <bo.tang3@56qq.com>
2019-01-24 15:57:56 +0800

    feat: 优化打包方式

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -3,5 +3,27 @@ module.exports = (config) => {
   config.disableAntdStyle = true;
   config.library = 'ffe-basic';
   config.libraryTarget = 'umd';
+  config.externals = {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+        amd: 'react',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+    },
+    antd: {
+      commonjs: 'antd',
+      commonjs2: 'antd',
+        amd: 'antd',
+    },
+    moment: {
+      commonjs: 'moment',
+      commonjs2: 'moment',
+        amd: 'moment',
+    },
+  }
   return config;
 };
