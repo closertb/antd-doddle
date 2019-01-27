4703efc79c20e1c5c3d43fa9b8f3413759656b28
closertb <closertb@sina.com>
2019-01-27 18:49:38 +0800

    feat: 新增webpack自定义打包

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'none',
+  mode: 'production',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+    library: 'antd-doddle',
+    libraryTarget: 'umd',
+    sourceMapFilename: '[file].map', // string
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+    },
+    antd: {
+      commonjs: 'antd',
+      commonjs2: 'antd',
+      amd: 'antd',
+    },
+    moment: {
+      commonjs: 'moment',
+      commonjs2: 'moment',
+      amd: 'moment',
+    },
+  },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['@babel/preset-env', '@babel/preset-react']
+      }
+    }, {
+      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000
+      }
+    }, {
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader'],
+    }, {
+      test: /\.scss$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+    }, {
+      test: /\.less$/,
+      use: [MiniCssExtractPlugin.loader, {
+        loader: 'css-loader',
+        options: {
+          // minimize: true,
+          modules: true,
+          localIndetName: '[local]_[hash:base64:5]'
+        }
+      }, 'less-loader']
+    }]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({
+      filename: './index.css' // 文件目录会放入output.path里
+    }),
+  ],
+};
