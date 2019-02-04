4075b2ce2d9ef73e29335da32ccb8c0b59757c3d
closertb <closertb@sina.com>
2019-02-04 11:33:06 +0800

    feat: 新增less复制打包

--- gulpfile.js
+++ gulpfile.js
@@ -12,16 +12,26 @@ gulp.task('cleanEs', () => {
   return clean(['es']);
 });
 
-// babel 打包es模块
-gulp.task('default', gulp.series('clean', () => {
-  return gulp.src('./src/**/*.js')
-    .pipe(babel())
+// 复制less模块到lib
+gulp.task('lessToLib', () => {  
+  return gulp.src('./src/**/*.less')
     .pipe(gulp.dest('./lib'));
-}));
+});
+
+// 复制less模块到es
+gulp.task('lessToEs', () => {
+  return gulp.src('./src/**/*.less')
+    .pipe(gulp.dest('./es'));
+});
+// babel 打包es模块
+gulp.task('lib', gulp.series('clean', () => {
+  return gulp.src('./src/**/*.js')
+    .pipe(gulp.dest('./lib'));
+}, 'lessToLib'));
 
 // babel 打包成支持es6模块的语法
 // 配置modules: false，保留es6模块化语法
-gulp.task('esModule', gulp.series('cleanEs', () => {
+gulp.task('es', gulp.series('cleanEs', () => {
   return gulp.src('./src/**/*.js')
     .pipe(babel({
       babelrc: false,
@@ -37,4 +47,13 @@ gulp.task('esModule', gulp.series('cleanEs', () => {
       ]
     }))
     .pipe(gulp.dest('./es'));
-}));
+}, 'lessToEs'));
+
+// 发布打包
+gulp.task('lib', gulp.series('clean', () => {
+  return gulp.src('./src/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./lib'));
+}, 'lessToLib'));
+
+gulp.task('default', gulp.parallel(['lib', 'es']));
